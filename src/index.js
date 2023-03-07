daaca40ecf4fe32042a9676be7f704605a0e0c28
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-07 14:09:35 +0330

    Add Inter Font to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/global.css'
+import './assets/style/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
