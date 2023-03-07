ea22f111ba1ba294d66833464bdb20259b5cc0ee
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-07 13:59:14 +0330

    Add icon to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { HiEmojiHappy } from "react-icons/hi";
+
 function App() {
   return (
     <div className="text-3xl text-red-700">
       Hello World 1
+      <HiEmojiHappy />
     </div>
   );
 }
