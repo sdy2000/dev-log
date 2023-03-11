028739535991b05cea738a89e748f3b83d973e29
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-11 15:11:01 +0330

    Update HomeIndex.jsx

--- src/pages/home/HomeIndex.jsx
+++ src/pages/home/HomeIndex.jsx
@@ -5,21 +5,21 @@ const HomeIndex = () => {
   return (
     <>
       <main>
-        <div className='hero'>
+        <div className='bg-lbp dark:bg-dbp pt-8 pb-2 text-center'>
           <div className='container'>
-            <div className='left'>
-              <h1 className='text-xl font-black'>
-                Hi, This is <b>DEVLog</b>.
+            <div className='left flex flex-col items-center gap-3'>
+              <h1 className='text-xl font-black text-lfp dark:text-dfp'>
+                Hi, This is <b className='text-accent'>DEVLog</b>.
                 <br />
                 Web Developer
               </h1>
-              <p className='text-base font-semibold'>Specialized in Full Stack Development</p>
-              <div className="btn-group">
+              <p className='text-base font-semibold text-lfp dark:text-dfp'>Specialized in Full Stack Development</p>
+              <div className="btn-group flex justify-center gap-3">
                 <Button1 value={'Contact Us'} href={'#'} />
                 <Button1 value={'About Us'} href={'#'} color={'bg-lfs hover:bg-lft text-white dark:bg-dfs dark:hover:bg-dft dark:text-black'} />
               </div>
             </div>
-            <div className="right">
+            <div className="right hidden">
               <div className="pattern-bg"></div>
               <div className="img-box">
                 <img src="/assets/img/about.png" alt="User Avatar Img" />
