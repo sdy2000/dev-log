df9050cc4a6c7d9e4525f402e99060914bd46573
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-11 16:46:40 +0330

    Update CardTime.jsx

--- src/components/showTime/CardTime.jsx
+++ src/components/showTime/CardTime.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {BiTime} from 'react-icons/bi'
 
-const CardTime = () => {
+const CardTime = ({dateTime}) => {
   return (
-    <div>CardTime</div>
+    <p className='text-xs font-semibold flex items-center gap-1'>
+      <time dateTime={dateTime}>{dateTime}</time>
+      <span className=''></span>
+      <BiTime />
+      <time dateTime='PT3M'>3 min</time>
+    </p>
   )
 }
 
