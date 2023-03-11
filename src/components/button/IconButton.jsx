aae6f22e93faa1fe24d84c9539bd16021d3dfbe3
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-11 12:12:51 +0330

    Updadte IconButton

--- src/components/button/IconButton.jsx
+++ src/components/button/IconButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const iconButton = ({value}) => {
+const iconButton = ({ value }) => {
   return (
-        <button>
-            {value}
-        </button>
+    <button className='flex justify-center items-center text-2xl border rounded-full w-10 h-10
+         text-lft bg-las hover:bg-accent hover:text-white
+          dark:text-dft dark:bg-das dark:hover:bg-accent dark:hover:text-white
+          transition-all duration-500'>
+      {value}
+    </button>
   )
 }
 
