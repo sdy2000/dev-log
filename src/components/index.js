c85df9fe7f47e3d6f3cac6ebdf6ba4f4662c7cfd
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-07-10 22:12:18 +0430

    Create AccountingInput and connect with api

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-// // // // Buttons // // // //
+// / // / // / // Buttons // / // / // / //
 export { default as IconButton } from "./button/IconButton";
 export { default as ThemeButton } from "./button/ThemeButton";
 export { default as Button1 } from "./button/Button1";
@@ -7,10 +7,13 @@ export { default as PageNumButton } from "./button/PageNumButton";
 export { default as NumberButton } from "./button/NumberButton";
 export { default as TopicButton } from "./button/TopicButton";
 
-// // // // Card // // // //
+// / // / // / // Card // / // / // / //
 export { default as BlogCard } from "./card/BlogCard";
 export { default as Aside } from "./card/Aside";
 export { default as SocialBox } from "./card/SocialBox";
 
-// // // // Time // // // //
+// / // / // / // Time // / // / // / //
 export { default as CardTime } from "./showTime/CardTime";
+
+// / // / // / // Inputs // / // / // / //
+export { default as AccountingInput } from "./inputs/AccountingInput";
--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -1,74 +1,112 @@
-import React, { useState } from 'react'
-import { BsEye, BsEyeSlash } from 'react-icons/bs'
-import { Button1 } from '../../components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { AccountingInput } from "../../components";
+import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
+import { ENDPOINTS, createAPIEndpoint } from "../../service/api";
 
+const getLoginModel = () => ({
+  email: "",
+  password: "",
+  remember_me: true,
+});
 const Login = () => {
-    const [isShowPass, setIsShowPass] = useState(false)
+  const { values, handleInputChange } = useForm(getLoginModel);
 
-    return (
-        <div className='mx-auto my-56 md:my-80 px-4 w-full max-w-lg'>
-            <div className='bg-lbp dark:bg-dbp w-full py-10 rounded-2xl shadow-lg flex flex-col justify-center items-center gap-8'>
-                <h1 className='text-lfp dark:text-dfp text-2xl font-bold'>
-                    Login
-                </h1>
-                <form className='flex flex-col items-center w-full'>
-                    <div className='form-group'>
-                        <input
-                            type='text'
-                            placeholder='User Name'
-                            required />
-                    </div>
-                    <p className='form-group-p'>This filed is requaird !</p>
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const login = Object.fromEntries(formData);
+    if (login.remember_me === undefined) {
+      login.remember_me = false;
+    } else {
+      login.remember_me = true;
+    }
 
-                    <div className='form-group'>
-                        <input
-                            className='rounded-l-xl'
-                            type={!isShowPass ? 'password' : 'text'}
-                            placeholder='Password'
-                            required />
-                        <span className='bg-lbs dark:bg-dbs text-lfp dark:text-dfp pr-4 hover:scale-110 cursor-pointer duration-300'>
-                            {
-                                !isShowPass
-                                    ?
-                                    <BsEye
-                                        onClick={() => { setIsShowPass(true) }}
-                                        size={30} />
-                                    :
-                                    <BsEyeSlash
-                                        onClick={() => { setIsShowPass(false) }}
-                                        size={30} />
-                            }
-                        </span>
-                    </div>
-                    <p className='form-group-p'>This filed is requaird !</p>
+    createAPIEndpoint(ENDPOINTS.login)
+      .post(login)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="mx-auto my-56 md:my-80 px-4 w-full max-w-lg">
+      <div className="bg-lbp dark:bg-dbp w-full py-10 rounded-2xl shadow-lg flex flex-col justify-center items-center gap-8">
+        <h1 className="text-lfp dark:text-dfp text-2xl font-bold">Login</h1>
+        <form
+          className="flex flex-col items-center gap-8 w-full px-12"
+          onSubmit={handleFormSubmit}
+        >
+          <AccountingInput
+            type={"email"}
+            name={"email"}
+            id={"email"}
+            placeholder={"Enter your email address..."}
+            onChange={handleInputChange}
+            value={values.email}
+          />
 
-                    <div className='flex'>
-                        <p className='text-lfs dark:text-dfs text-sm font-semibold'>
-                            If forgot your password click here
-                        </p>
-                        <b className='text-accent hover:underline ml-2'>
-                            <Link to='#'>
-                                Forgot Password
-                            </Link>
-                        </b>
-                    </div>
-                    <div className='flex'>
-                        <p className='text-lfs dark:text-dfs text-sm font-semibold'>
-                            If you do not have an account,
-                        </p>
-                        <b className='text-accent hover:underline ml-2'>
-                            <Link to='/register'>
-                                Create One Here
-                            </Link>
-                        </b>
-                    </div>
+          <AccountingInput
+            type={"password"}
+            name={"password"}
+            id={"password"}
+            placeholder={"Enter your password..."}
+            onChange={handleInputChange}
+            value={values.password}
+            hasIcon={true}
+          />
+          <div className="flex justify-start items-center gap-2 w-full">
+            <input
+              name="remember_me"
+              onChange={handleInputChange}
+              value={values.remember_me}
+              type="checkbox"
+              className="w-4 h-4"
+              id="rules"
+            />
+            <label
+              className="text-lfp dark:text-dfp text-sm font-semibold"
+              htmlFor="rules"
+            >
+              I agree to{" "}
+              <Link
+                to="#"
+                className="text-accent hover:text-daccent duration-300 mx-1 font-bold text-lg"
+              >
+                rules
+              </Link>{" "}
+              from ECrypto
+            </label>
+          </div>
 
-                    <Button1 value={'Login'} color='bg-accent text-white hover:bg-lft hover:text-lfp hover:border-lft border-accent px-8 mt-6' />
-                </form>
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-start items-center">
+              <p className="text-lfs dark:text-dfs text-sm font-semibold">
+                If forgot your password click here
+              </p>
+              <b className="text-accent hover:underline ml-2">
+                <Link to="#">Forgot Password</Link>
+              </b>
             </div>
-        </div>
-    )
-}
+            <div className="flex justify-start items-center">
+              <p className="text-lfs dark:text-dfs text-sm font-semibold">
+                If you do not have an account,
+              </p>
+              <b className="text-accent hover:underline ml-2">
+                <Link to="/register">Create One Here</Link>
+              </b>
+            </div>
+          </div>
 
-export default Login
+          <input
+            className="bg-accent text-white hover:bg-daccent border-accent px-8 mt-6 text-sm font-semibold md:text-lg md:font-bold hover:-translate-y-2 hover:scale-x-110 rounded-3xl py-2 transition-all duration-300 border-2 flex items-center justify-center"
+            type="submit"
+            value="Log In"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
