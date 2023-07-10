56206be9d41c3f19c12c05694245f298381ce0fd
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-07-10 21:02:13 +0430

    Create api for back-end service and create useForm hook

---
+++ src/service/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const BASE_URL = "https://localhost:7244/";
+
+export const ENDPOINTS = {
+  register: "account/register",
+  login: "account/login",
+};
+
+export const createAPIEndpoint = (endpoint) => {
+  let url = BASE_URL + "api/" + endpoint + "/";
+
+  return {
+    fetch: () => axios.get(url),
+    fetchById: (id) => axios.get(url + id),
+    post: (newRecord) => axios.post(url, newRecord),
+    put: (id, updateRecord) => axios.put(url + id, updateRecord),
+    delete: (id) => axios.delete(url + id),
+  };
+};
